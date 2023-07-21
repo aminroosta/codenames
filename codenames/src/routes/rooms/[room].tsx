f0d0e4da85da5261f3a6bda6dbbb189da7ff156a
amin <amin.roosta@outlook.com>
2023-07-21 16:44:37 -0400

    Clue

--- codenames/src/routes/rooms/[room].tsx
+++ codenames/src/routes/rooms/[room].tsx
@@ -1,8 +1,11 @@
 import { Show } from "solid-js";
 import { createStore } from "solid-js/store";
 import { createRouteData, useLocation, useRouteData } from "solid-start";
-import type { UserRole, Room } from "~/common/types";
 import Board from "~/components/Board";
+import type { UserRole, Room } from "~/common/types";
+import Team from "~/components/Team";
+import "./room.css";
+import Clue from "~/components/Clue";
 
 export function routeData() {
   return createRouteData(async () => {
@@ -25,12 +28,39 @@ export default function Room() {
 function RoomImpl(p: { room: Room }) {
   const [state, setState] = createStore({
     clue: { word: '', count: 0, votes: [] },
-    role: 'none' as UserRole,
+    role: 'red-spymaster' as UserRole,
     cards: p.room.cards.map(c => ({ ...c, face: 'down' })),
     status: p.room.status,
   });
 
-  return <div>
-    <Board {...state} />
+  const cardCount = (color: 'red' | 'blue') => state.cards.filter(c => c.color === color).length;
+  const onClue = (clue: { word: string, count: number }) => {
+    console.log(clue);
+  };
+
+  return <div class='room'>
+    <TeamImpl color="red" cardCount={cardCount('red')} />
+    <div class="board-wrapper">
+      <div class='status'> {state.status} </div>
+      <Board {...state} />
+      <Show when={state.role.includes('spymaster')}>
+        <Clue onDone={onClue} />
+      </Show>
+    </div>
+    <TeamImpl color="blue" cardCount={cardCount('blue')} />
   </div>;
+}
+
+function TeamImpl(p: { color: "red" | "blue", cardCount: number }) {
+  const [state, setState] = createStore({
+    spymasters: [],
+    operatives: [],
+    cardCount: p.cardCount,
+    role: "none" as "none" | "operator" | "spymaster",
+    onJoin: (role: "operator" | "spymaster") => {
+      setState("role", role);
+    },
+  });
+
+  return <Team {...state} color={p.color} />
 }
