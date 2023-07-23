ce0473f9ee0a665bc2e70a34d7f403912e8987b7
amin <amin.roosta@outlook.com>
2023-07-23 00:41:01 -0400

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [solid({ ssr: false })],
+  ssr: {
+    external: ["ws", "uuid", "bip39", "better-sqlite3"],
+  }
 });
